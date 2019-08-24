72e63c4f80fcae2d518d4f28bc29c695392b3098
Tu Nguyen <tu.t.nguyen@uwaterloo.ca>
2019-08-24 11:27:05 -0400

    feat: remove unnecessary files and create board folder structure

---
+++ ng-app/src/app/board/board.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { FogService } from './fog.service';
+
+@NgModule({
+    declarations: [],
+    exports: [],
+    providers: [FogService]
+})
+export class BoardModule {
+
+}
